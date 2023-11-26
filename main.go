8675e88c3b5d99f852a6ef786e258fcfc83aacba
pecet3 <pecet3107@gmail.com>
2023-11-26 17:23:37 +0100

    handle ws server

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	setupAPI()
+	log.Println("> Starting the server...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func setupAPI() {
-	http.Handle("/", http.FileServer(http.Dir("./frontend")))
-	http.ListenAndServe(":8080", nil)
 
+	manager := NewManager()
+
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/ws", manager.serveWS)
 }
