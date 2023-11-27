8a577f4ecdf4ae8146823fcfeb462f2e12df9974
pecet3 <pecet3107@gmail.com>
2023-11-27 01:19:53 +0100

    egress chanel

--- client.go
+++ client.go
@@ -11,25 +11,65 @@ type ClientList map[*Client]bool
 type Client struct {
 	connection *websocket.Conn
 	manager    *Manager
+
+	//egress is use to avoid problems with concurrent writes
+
+	egress chan []byte
 }
 
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
 		manager:    manager,
+		egress:     make(chan []byte),
 	}
 }
 
 func (c *Client) readMeassages() {
+
+	defer func() {
+		// clean up connection
+		c.manager.removeClient(c)
+	}()
+
 	for {
 		messageType, payload, err := c.connection.ReadMessage()
 
 		if err != nil {
-
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, we) {
-				log.Println(err)
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error reading msg: %v", err)
 			}
+			break
+		}
 
+		for wsclient := range c.manager.clients {
+			wsclient.egress <- payload
+		}
+
+		log.Println(messageType)
+		log.Println(payload)
+	}
+
+}
+
+func (c *Client) writeMessages() {
+	defer func() {
+		c.manager.removeClient(c)
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.egress:
+			if !ok {
+				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					log.Println("connection closed: ", err)
+				}
+				return
+			}
+			if err := c.connection.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Println("failed to send message: ", err)
+			}
+			log.Println("message has sent: ", message)
 		}
 	}
 }
